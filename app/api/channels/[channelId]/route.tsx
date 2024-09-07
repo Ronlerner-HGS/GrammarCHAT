4ada6a2fa8613707468f252440db16c68bc554c6
Ron Lerner <97751296+Ronlerner-HGS@users.noreply.github.com>
2024-09-07 13:51:22 -0300

    :(

--- app/api/channels/[channelId]/route.tsx
+++ app/api/channels/[channelId]/route.tsx
@@ -4,95 +4,106 @@ import { MemberRole } from "@prisma/client";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-export async function PATCH(req: Request) {
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
   try {
     const profile = await currentProfile();
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
 
     const { searchParams } = new URL(req.url);
     const serverId = searchParams.get("serverId");
-    const channelId = searchParams.get("channelId");
-    if (!serverId) return new NextResponse("Server ID Missing", { status: 400 });
-    if (!channelId) return new NextResponse("Channel ID Missing", { status: 400 });
+    if (!serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+    if (!params.channelId)
+      return new NextResponse("Channel ID Missing", { status: 400 });
 
     const { name, type } = await req.json();
     if (!name || !type || name === "general")
-      return new NextResponse("Name / Type cannot be empty or general", { status: 400 });
+      return new NextResponse("Name / Type cannot be empty or general", {
+        status: 400
+      });
 
-    const server = await db.server.findUnique({
-      where: { id: serverId },
-    });
-
-    if (!server) return new NextResponse("Server not found", { status: 404 });
-
-    const isMember = await db.member.findFirst({
+    const server = await db.server.update({
       where: {
-        serverId: server.id,
-        profileId: profile.id,
-        role: { in: [MemberRole.ADMIN, MemberRole.MODERATOR] },
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+            }
+          }
+        }
       },
-    });
-
-    if (!isMember) return new NextResponse("Forbidden", { status: 403 });
-
-    const updatedServer = await db.server.update({
-      where: { id: serverId },
       data: {
         channels: {
           update: {
-            where: { id: channelId },
-            data: { name, type },
-          },
-        },
-      },
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: "general"
+              }
+            },
+            data: {
+              name,
+              type
+            }
+          }
+        }
+      }
     });
 
-    return NextResponse.json(updatedServer);
+    return NextResponse.json(server);
   } catch (error) {
-    console.error("[CHANNEL_PATCH]", error);
+    console.error("[CHANNEL_ID_PATCH", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
 
-export async function DELETE(req: Request) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
   try {
     const profile = await currentProfile();
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
 
     const { searchParams } = new URL(req.url);
     const serverId = searchParams.get("serverId");
-    const channelId = searchParams.get("channelId");
-    if (!serverId) return new NextResponse("Server ID Missing", { status: 400 });
-    if (!channelId) return new NextResponse("Channel ID Missing", { status: 400 });
+    if (!serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+    if (!params.channelId)
+      return new NextResponse("Channel ID Missing", { status: 400 });
 
-    const server = await db.server.findUnique({
-      where: { id: serverId },
-    });
-
-    if (!server) return new NextResponse("Server not found", { status: 404 });
-
-    const isMember = await db.member.findFirst({
+    const server = await db.server.update({
       where: {
-        serverId: server.id,
-        profileId: profile.id,
-        role: { in: [MemberRole.ADMIN, MemberRole.MODERATOR] },
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+            }
+          }
+        }
       },
-    });
-
-    if (!isMember) return new NextResponse("Forbidden", { status: 403 });
-
-    const updatedServer = await db.server.update({
-      where: { id: serverId },
       data: {
         channels: {
-          delete: { id: channelId },
-        },
-      },
+          delete: {
+            id: params.channelId,
+            name: {
+              not: "general"
+            }
+          }
+        }
+      }
     });
 
-    return NextResponse.json(updatedServer);
+    return NextResponse.json(server);
   } catch (error) {
-    console.error("[CHANNEL_DELETE]", error);
+    console.error("[CHANNEL_ID_DELETE", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
