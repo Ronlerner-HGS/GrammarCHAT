f202a5e308e28c06db9151c34f10ce83e2d95f14
Ron Lerner <97751296+Ronlerner-HGS@users.noreply.github.com>
2024-09-07 13:14:43 -0300

    ()::

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,21 +1,31 @@
+import React from "react";
 import { redirectToSignIn } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { ServerSidebar } from "@/components/server/server-sidebar";
 
-interface ServerIdPageProps {
-  params: {
-    serverId: string;
-  };
-}
-
-export default async function ServerIdPage({ params }: ServerIdPageProps) {
+export default async function ServerPage({
+                                           children,
+                                           params
+                                         }: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) {
   const profile = await currentProfile();
 
   if (!profile) return redirectToSignIn();
 
   const server = await db.server.findUnique({
+    where: {
+      id: params.serverId
+    }
+  });
+
+  if (!server) return redirect("/");
+
+  const isMember = await db.server.findFirst({
     where: {
       id: params.serverId,
       members: {
@@ -23,20 +33,17 @@ export default async function ServerIdPage({ params }: ServerIdPageProps) {
           profileId: profile.id
         }
       }
-    },
-    include: {
-      channels: {
-        where: {
-          name: "general"
-        },
-        orderBy: { createdAt: "asc" }
-      }
     }
   });
 
-  const initialChannel = server?.channels[0];
+  if (!isMember) return redirect("/");
 
-  if (initialChannel?.name !== "general") return null;
-
-  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+  return (
+      <div className="h-full">
+        <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+          <ServerSidebar serverId={params.serverId} />
+        </div>
+        <main className="h-full md:pl-60">{children}</main>
+      </div>
+  );
 }
