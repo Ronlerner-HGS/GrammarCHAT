212e2eedc923f099e82d47238e1b16a732f1f40e
Ron Lerner <97751296+Ronlerner-HGS@users.noreply.github.com>
2024-09-07 12:59:26 -0300

    pls work or else [gun_emoji]

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { redirect } from "next/navigation";
-
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { InitialModal } from "@/components/modals/initial-modal";
@@ -8,17 +7,23 @@ import { InitialModal } from "@/components/modals/initial-modal";
 export default async function SetupPage() {
   const profile = await initialProfile();
 
-  const server = await db.server.findFirst({
-    where: {
-      members: {
-        some: {
-          profileId: profile.id
+  // Type guard to check if profile is the expected object
+  if ('id' in profile) {
+    const server = await db.server.findFirst({
+      where: {
+        members: {
+          some: {
+            profileId: profile.id
+          }
         }
       }
-    }
-  });
+    });
 
-  if (server) return redirect(`/servers/${server.id}`);
+    if (server) return redirect(`/servers/${server.id}`);
+  } else {
+    // Handle the case where profile is not the expected object
+    return redirect('/sign-in');
+  }
 
   return <InitialModal />;
 }
