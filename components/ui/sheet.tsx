61ec099a995997c0f6b0dd27ddf42bef7c099ad7
Ron Lerner <97751296+Ronlerner-HGS@users.noreply.github.com>
2024-09-08 22:00:59 -0300

    gonna use docker next time

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -117,7 +117,7 @@ const SheetTitle = React.forwardRef<
   />
 ))
 SheetTitle.displayName = SheetPrimitive.Title.displayName
-// cejfhurehtgiyerugh
+// nigga
 const SheetDescription = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
